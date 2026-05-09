0130c069a125066c8c7a34c5d30c8a1de4a811c9
SaimAhmad7 <zdnrvsqk22@privaterelay.appleid.com>
2026-05-09 21:04:51 +0400

    Add files via upload

---
+++ pointalism_(portfolio)_2026_05_09_17_03_49/sketch.js
@@ -0,0 +1,24 @@
+var img, x, y;
+function preload() {
+img = loadImage("scene.jpg");
+
+}
+
+
+function setup() {
+createCanvas (780, 438);
+background(0);
+noStroke();
+
+}
+
+function draw() {
+x = random(width);
+y = random(height);
+  
+var c = img.get(x, y);
+  
+fill(c[0], c[1], c[2], 50);
+ellipse (x, y, 30, 30);
+
+}
